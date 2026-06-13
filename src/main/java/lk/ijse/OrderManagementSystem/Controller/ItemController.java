aeca1930b9aeaf06d5988dbf502a4ceac28297c9
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-06-13 23:54:08 +0530

    Add item retrieval and update functionalities in ItemController and ItemServiceImpl

--- src/main/java/lk/ijse/OrderManagementSystem/Controller/ItemController.java
+++ src/main/java/lk/ijse/OrderManagementSystem/Controller/ItemController.java
@@ -5,10 +5,9 @@ import lk.ijse.OrderManagementSystem.DTO.ItemDTO;
 import lk.ijse.OrderManagementSystem.Service.ItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static lk.ijse.OrderManagementSystem.Constant.ResponseMessage.SUCCESS_MESSAGE;
 import static lk.ijse.OrderManagementSystem.Constant.ResponseStatusCode.OPERATION_SUCCESS;
@@ -30,6 +29,27 @@ public class ItemController {
         log.info("ItemController - saveItem() called");
         ItemDTO itemDTO1 = itemService.saveItem(itemDTO);
         return new CommonResponse(OPERATION_SUCCESS, itemDTO1, SUCCESS_MESSAGE);
+    }
+
+    @GetMapping(value = "/getAll",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse getAllItems() {
+        log.info("ItemController - getAllItems() called");
+        List<ItemDTO> itemDTOList = itemService.getAllItems();
+        return new CommonResponse(OPERATION_SUCCESS, itemDTOList, SUCCESS_MESSAGE);
+    }
+
+    @GetMapping(value = "/getById/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse getItemById(@PathVariable long id){
+        log.info("ItemController - getItemById() called with ID: {}", id);
+        ItemDTO itemDTO = itemService.getItemDetails(id);
+        return new CommonResponse(OPERATION_SUCCESS, itemDTO, SUCCESS_MESSAGE);
+    }
+
+    @PostMapping(value = "/update",produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse updateItem(@RequestBody ItemDTO itemDTO) {
+        log.info("ItemController - updateItem() called with ID: {}", itemDTO.getItemId());
+        ItemDTO updatedItemDTO = itemService.updateItem(itemDTO);
+        return new CommonResponse(OPERATION_SUCCESS, updatedItemDTO, SUCCESS_MESSAGE);
     }
 
 }
--- src/main/java/lk/ijse/OrderManagementSystem/Service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/OrderManagementSystem/Service/impl/ItemServiceImpl.java
@@ -7,7 +7,9 @@ import lk.ijse.OrderManagementSystem.Service.ItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -22,32 +24,93 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemDTO saveItem(ItemDTO itemDTO) {
-        Item item = new Item();
-        item.setItemName(itemDTO.getItemName());
-        item.setUnitPrice(itemDTO.getUnitPrice());
+        log.info("ItemServiceImpl - saveItem() called");
+        try {
+            Item item = new Item();
+            item.setItemName(itemDTO.getItemName());
+            item.setUnitPrice(itemDTO.getUnitPrice());
 
-        Item saveItem = itemRepository.save(item);
+            Item saveItem = itemRepository.save(item);
+            log.info("Item saved with ID: {}", saveItem.getItemId());
 
-        ItemDTO responseDto = new ItemDTO();
-        responseDto.setItemId(saveItem.getItemId());
-        responseDto.setItemName(saveItem.getItemName());
-        responseDto.setUnitPrice(saveItem.getUnitPrice());
+            ItemDTO responseDto = new ItemDTO();
+            responseDto.setItemId(saveItem.getItemId());
+            responseDto.setItemName(saveItem.getItemName());
+            responseDto.setUnitPrice(saveItem.getUnitPrice());
 
-        return responseDto;
+            return responseDto;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<ItemDTO> getAllItems() {
-        return List.of();
+        log.info("ItemServiceImpl - getAllItems() called");
+        try {
+            List<ItemDTO> itemDTOList = new ArrayList<>();
+
+            List<Item> items = itemRepository.findAll();
+            log.info("Total items found: {}", items.size());
+
+            for (Item item: items){
+                ItemDTO itemDTO = new ItemDTO();
+                itemDTO.setItemId(item.getItemId());
+                itemDTO.setItemName(item.getItemName());
+                itemDTO.setUnitPrice(item.getUnitPrice());
+                itemDTOList.add(itemDTO);
+            }
+            return itemDTOList;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ItemDTO getItemDetails(long id) {
-        return null;
+        log.info("ItemServiceImpl - getItemDetails() called with ID: {}", id);
+        try {
+            Optional<Item> optionalItem = itemRepository.findById(id);
+            if (!optionalItem.isPresent()) {
+                throw new RuntimeException("Item not found with ID: " + id);
+            }
+            Item item = optionalItem.get();
+            log.info("Item found: {}", item.getItemName());
+
+            ItemDTO responseDTO = new ItemDTO();
+            responseDTO.setItemId(item.getItemId());
+            responseDTO.setItemName(item.getItemName());
+            responseDTO.setUnitPrice(item.getUnitPrice());
+            return responseDTO;
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ItemDTO updateItem(ItemDTO itemDTO) {
-        return null;
+
+        log.info("ItemServiceImpl - updateItem() called with ID: {}", itemDTO.getItemId());
+        try {
+            Optional<Item> optionalItem = itemRepository.findById(itemDTO.getItemId());
+            if (!optionalItem.isPresent()) {
+                throw new RuntimeException("Item not found with ID: " + itemDTO.getItemId());
+            }
+            Item item = optionalItem.get();
+            item.setItemName(itemDTO.getItemName());
+            item.setUnitPrice(itemDTO.getUnitPrice());
+            Item updateItem = itemRepository.save(item);
+            log.info("Item updated with ID: {}", item.getItemId());
+
+            ItemDTO responseDTO = new ItemDTO();
+            responseDTO.setItemId(updateItem.getItemId());
+            responseDTO.setItemName(updateItem.getItemName());
+            responseDTO.setUnitPrice(updateItem.getUnitPrice());
+            return responseDTO;
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
